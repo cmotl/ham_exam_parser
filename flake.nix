6630c578b5969d1af69974e9a15e43ffed914f28
Christopher Motl <cmotl@cmotl.com>
2026-02-15 22:08:07 -0600

    Convert from shell.nix to flake for direnv compatibility

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Ham Exam Parser - ARRL question pool parser";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            ruby_3_3
+            pkg-config
+            libxml2
+            libxslt
+            zlib
+            librsvg
+          ];
+
+          shellHook = ''
+            export GEM_HOME="$PWD/vendor/bundle"
+            export GEM_PATH="$GEM_HOME"
+            export PATH="$GEM_HOME/bin:$PATH"
+            export BUNDLE_PATH="$GEM_HOME"
+          '';
+        };
+      });
+}
